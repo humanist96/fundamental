be9864b62faaf5f2c3c6594375f0f513a8c74a9e
humanist96 <humanist96@gmail.com>
2024-04-25 09:37:54 +0900

    Update 🏠_home.py

--- 🏠_home.py
+++ 🏠_home.py
@@ -10,12 +10,14 @@ st.write("# Welcome to 부동산 & 주식 데이터 분석! 👋")
 st.sidebar.success("Select a menu above.")
 
 st.markdown(
-    ""  
+    """ 
+
     **👈 Select a menu from the sidebar** 
 
     ### Menu 설명
     - 📆 apt weekly : 한국부동산원과 KB에서 매주 발표하는 부동산 지수를 다양한 차트와 테이블로 아파트 주간 동향을 살펴 봄
     - 🈷️ apt monthly : 한국부동산원과 KB에서 매달 발표하는 부동산 지수를 다양한 차트와 테이블로 아파트 매월 동향을 살펴 봄
+    - 📊_apt_real.py : 한국부동산원과 KB에서 매달 발표하는 설거래가 지수를 다양한 차트와 테이블로 아파트 매월 동향을 살펴 봄
     - 🏗️ rebuild house : 네이버 부동산에서 가져온 전국의 아파트분양권, 재개발, 재건축 매물 위치와 정보 확인
     - 🪙 macro : 국내, 미국 주요 매크로 차트 보기
     - ⚛️ korchart : 국내 상장 주식의 기대수익률과 기본적인 재무 데이터 차트 보기
